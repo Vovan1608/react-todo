cb0df458e01db4530844912d1f7371963ee4ef3a
Vladymyr Zenin <vzeni@softserveinc.com>
2022-06-30 17:38:17 +0300

    add JSX examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('app');
+const root = createRoot(container);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const TodoList = () => {
+    const items = ['Learn React', 'Make awasome app'];
+    return (
+        <ul>
+            <li>{items[0]}</li>
+            <li>{items[1]}</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return (
+        <h1> My ToDo List</h1>
+    );
+};
+
+const SearchPannel = () => {
+    const tipeSearch = 'Tipe here to search';
+
+    const searchStyle = {
+        fontSize: '25px'
+    }
+
+    return (
+        <input
+            style={searchStyle}
+            placeholder={tipeSearch}
+            disabled
+        />
+    );
+};
+
+const App = () => {
+    const valee = '<script>alert("This is going very poorly")</script>';
+
+    return (
+        <>
+            <div>{valee}</div>
+            <AppHeader />
+            <SearchPannel />
+            <TodoList />
+        </>
+    );
+
+};
+
+root.render(<App />);
